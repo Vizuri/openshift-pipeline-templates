9dc58d301cd3cbabea1a358a5f4a3ff76bf78739
Kent Eudy <keudy@vizuri.com>
2018-09-27 14:51:18 -0400

    Updates

--- src/com/vizuri/openshift/DeliveryPipeline.groovy
+++ src/com/vizuri/openshift/DeliveryPipeline.groovy
@@ -42,9 +42,23 @@ def call(body) {
               echo "In checkout" 
     	      checkout scm
          }
-         stage('MavenBuild') {
+         stage('Build') {
               echo "In Build"		
-              sh "mvn -s configuration/settings.xml -Dbuild.number=${release_number} clean install"
+              sh "mvn -s configuration/settings.xml -DskipTests=true -Dbuild.number=${release_number} clean install"
+         }
+
+         stage ('test') {
+             steps {
+                parallel (
+                   "unit tests": { sh 'mvn -s configuration/settings.xml -Dbuild.number=${release_number} test' },
+                   "integration tests": { sh 'mvn -s configuration/settings.xml -Dbuild.number=${release_number} integration-test' }
+                )
+                junit 'target/surefire-reports/*.xml'
+
+                step([$class: 'XUnitBuilder',
+                   thresholds: [[$class: 'FailedThreshold', unstableThreshold: '1']],
+                   tools: [[$class: 'JUnitType', pattern: 'target/surefire-reports/*.xml']]])
+             }
          }
 
          stage('DockerBuild') {
