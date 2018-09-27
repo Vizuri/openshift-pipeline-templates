010ee8106fc68efc5e9bf546c35c959518087bce
Kent Eudy <keudy@vizuri.com>
2018-09-27 12:43:11 -0400

    Updates

--- src/com/vizuri/openshift/DeliveryPipeline.groovy
+++ src/com/vizuri/openshift/DeliveryPipeline.groovy
@@ -51,12 +51,12 @@ def call(body) {
              echo "In DockerBuild: ${pipelineParams.ocp_cluster} : ${ocp_project}" 
 	     openshift.withCluster( "${pipelineParams.ocp_cluster}" ) {
 	          openshift.withProject( "${ocp_project}" ) {
-		      def bc = openshift.selector("bc", "${app_name}")
+		      def bc = openshift.selector("bc", "${pipelineParams.app_name}")
 		      echo "BC: " + bc
 		      echo "BC Exists: " + bc.exists()
 		      if(!bc.exists()) {
 		         echo "BC Does Not Exist Creating"
-			 bc = openshift.newBuild("--binary=true --strategy=docker --name=${app_name}")
+			 bc = openshift.newBuild("--binary=true --strategy=docker --name=${pipelineParams.app_name}")
 		      }
 		      bc = bc.narrow("bc");
 		      bc.startBuild("--from-dir .")
@@ -67,12 +67,12 @@ def call(body) {
 	     openshift.withCluster( "${pipelineParams.ocp_cluster}" ) {
 	         openshift.withProject( "${ocp_project}" ) {
 		      echo "In Deploy: ${openshift.project()} : ${ocp_project}"
-		      def dc = openshift.selector("dc", "${app_name}")
+		      def dc = openshift.selector("dc", "${pipelineParams.app_name}")
 		      echo "DC: " + dc
 		      echo "DC Exists: " + dc.exists()
 		      if(!dc.exists()) {
 		            echo "DC Does Not Exist Creating"
-		            dc = openshift.newApp("-f https://raw.githubusercontent.com/Vizuri/openshift-pipeline-templates/master/templates/springboot-dc.yaml -p IMAGE_NAME=172.30.1.1:5000/${ocp_project}/${app_name}:latest -p APP_NAME=${app_name}")
+		            dc = openshift.newApp("-f https://raw.githubusercontent.com/Vizuri/openshift-pipeline-templates/master/templates/springboot-dc.yaml -p IMAGE_NAME=172.30.1.1:5000/${ocp_project}/${pipelineParams.app_name}:latest -p APP_NAME=${pipelineParams.app_name}")
 		      }
 		      else {
 		            dc = dc.narrow("dc")
