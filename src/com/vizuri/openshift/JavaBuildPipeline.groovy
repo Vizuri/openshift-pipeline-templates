06d9ed8dc595218569fbc2b6768672391584ea2d
Kent Eudy <keudy@vizuri.com>
2018-10-02 08:01:20 -0400

    Updates

--- src/com/vizuri/openshift/JavaBuildPipeline.groovy
+++ src/com/vizuri/openshift/JavaBuildPipeline.groovy
@@ -1,32 +1,14 @@
 package com.vizuri.openshift
 
 def call(body) {
-    def pipelineParams= [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = pipelineParams
-    body()
+	def pipelineParams= [:]
+	body.resolveStrategy = Closure.DELEGATE_FIRST
+	body.delegate = pipelineParams
+	body()
 
-      
-      node('maven') {	
-         stage('Checkout') {
-              echo "In checkout" 
-    	      checkout scm
-         }
-         stage('Build') {
-              echo "In Build"		
-              sh "mvn -s configuration/settings.xml -DskipTests=true -Dbuild.number=${release_number} clean install"
-         }
 
-         stage ('test') {
-                parallel (
-                   "unit tests": { sh 'mvn -s configuration/settings.xml -Dbuild.number=${release_number} test' },
-                   "integration tests": { sh 'mvn -s configuration/settings.xml -Dbuild.number=${release_number} integration-test' }
-                )
-                junit 'target/surefire-reports/*.xml'
-
-                step([$class: 'XUnitBuilder',
-                   thresholds: [[$class: 'FailedThreshold', unstableThreshold: '1']],
-                   tools: [[$class: 'JUnitType', pattern: 'target/surefire-reports/*.xml']]])
-         }
-     }
-  }
+	node('maven') {
+		utils.buildJava(release_number)
+		utils.testJava(release_number)
+	}
+}
--- src/com/vizuri/openshift/Utils.groovy
+++ src/com/vizuri/openshift/Utils.groovy
@@ -2,74 +2,88 @@ package com.vizuri.openshift
 
 
 def helloWorld() {
-   println("helloworkd");
+	println("helloworkd");
 }
 
 
 def buildJava(release_number) {
-      echo "In buildJava: ${release_number}"
-      stage('Checkout') {
-           echo "In checkout"
-           checkout scm
-      }
-      stage('Build') {
-           echo "In Build"
-           sh "mvn -s configuration/settings.xml -DskipTests=true -Dbuild.number=${release_number} clean install"
-      }
+	echo "In buildJava: ${release_number}"
+	stage('Checkout') {
+		echo "In checkout"
+		checkout scm
+	}
+	stage('Build') {
+		echo "In Build"
+		sh "mvn -s configuration/settings.xml -DskipTests=true -Dbuild.number=${release_number} clean install"
+	}
+}
+def testJava(release_number) {
+	echo "In testJava: ${release_number}"
+	stage ('test') {
+		parallel (
+				"unit tests": { sh 'mvn -s configuration/settings.xml -Dbuild.number=${release_number} test' },
+				"integration tests": { sh 'mvn -s configuration/settings.xml -Dbuild.number=${release_number} integration-test' }
+				)
+		junit 'target/surefire-reports/*.xml'
 
-      stage ('test') {
-             parallel (
-                "unit tests": { sh 'mvn -s configuration/settings.xml -Dbuild.number=${release_number} test' },
-                "integration tests": { sh 'mvn -s configuration/settings.xml -Dbuild.number=${release_number} integration-test' }
-             )
-             junit 'target/surefire-reports/*.xml'
-
-             step([$class: 'XUnitBuilder',
-                thresholds: [[$class: 'FailedThreshold', unstableThreshold: '1']],
-                tools: [[$class: 'JUnitType', pattern: 'target/surefire-reports/*.xml']]])
-      }
+		step([$class: 'XUnitBuilder',
+			thresholds: [
+				[$class: 'FailedThreshold', unstableThreshold: '1']
+			],
+			tools: [
+				[$class: 'JUnitType', pattern: 'target/surefire-reports/*.xml']
+			]])
+	}
 }
 
+def deployJava(release_number) {
+	echo "In buildJava: ${release_number}"
+
+	stage('Deploy Jave') {
+		echo "In Deploy"
+		sh "mvn -s configuration/settings.xml -DskipTests=true -Dbuild.number=${release_number} deploy"
+	}
+}
 
 def dockerBuildOpenshift(ocp_cluster, ocp_project, app_name) {
-         stage('DockerBuild') {
-             echo "In DockerBuild: ${ocp_cluster} : ${ocp_project}"
-             openshift.withCluster( "${ocp_cluster}" ) {
-                  openshift.withProject( "${ocp_project}" ) {
-                      def bc = openshift.selector("bc", "${app_name}")
-                      echo "BC: " + bc
-                      echo "BC Exists: " + bc.exists()
-                      if(!bc.exists()) {
-                         echo "BC Does Not Exist Creating"
-                         bc = openshift.newBuild("--binary=true --strategy=docker --name=${pipelineParams.app_name}")
-                      }
-                      bc = bc.narrow("bc");
-                      bc.startBuild("--from-dir .")
-                      bc.logs('-f')
-                  }
-              }
-          }
+	stage('DockerBuild') {
+		echo "In DockerBuild: ${ocp_cluster} : ${ocp_project}"
+		openshift.withCluster( "${ocp_cluster}" ) {
+			openshift.withProject( "${ocp_project}" ) {
+				def bc = openshift.selector("bc", "${app_name}")
+				echo "BC: " + bc
+				echo "BC Exists: " + bc.exists()
+				if(!bc.exists()) {
+					echo "BC Does Not Exist Creating"
+					bc = openshift.newBuild("--binary=true --strategy=docker --name=${pipelineParams.app_name}")
+				}
+				bc = bc.narrow("bc");
+				bc.startBuild("--from-dir .")
+			}
+			bc.logs('-f')
+		}
+	}
 }
 
 def deployOpenshift(ocp_cluster, ocp_project, app_name) {
-          stage('Deploy') {
-             openshift.withCluster( "${ocp_cluster}" ) {
-                 openshift.withProject( "${ocp_project}" ) {
-                      echo "In Deploy: ${openshift.project()} : ${ocp_project}"
-                      def dc = openshift.selector("dc", "${app_name}")
-                      echo "DC: " + dc
-                      echo "DC Exists: " + dc.exists()
-                      if(!dc.exists()) {
-                            echo "DC Does Not Exist Creating"
-                            dc = openshift.newApp("-f https://raw.githubusercontent.com/Vizuri/openshift-pipeline-templates/master/templates/springboot-dc.yaml -p IMAGE_NAME=172.30.1.1:5000/${ocp_project}/${app_name}:latest -p APP_NAME=${app_name}")
-                      }
-                      else {
-                            //dc = dc.narrow("dc")
-                            def ro = dc.rollout().latest()
-                            //dc.logs('-f')
-                      }
-                }
-             }
-          }      
+	stage('Deploy') {
+		openshift.withCluster( "${ocp_cluster}" ) {
+			openshift.withProject( "${ocp_project}" ) {
+				echo "In Deploy: ${openshift.project()} : ${ocp_project}"
+				def dc = openshift.selector("dc", "${app_name}")
+				echo "DC: " + dc
+				echo "DC Exists: " + dc.exists()
+				if(!dc.exists()) {
+					echo "DC Does Not Exist Creating"
+					dc = openshift.newApp("-f https://raw.githubusercontent.com/Vizuri/openshift-pipeline-templates/master/templates/springboot-dc.yaml -p IMAGE_NAME=172.30.1.1:5000/${ocp_project}/${app_name}:latest -p APP_NAME=${app_name}")
+				}
+				else {
+					//dc = dc.narrow("dc")
+					def ro = dc.rollout().latest()
+					//dc.logs('-f')
+				}
+			}
+		}
+	}
 }
 
