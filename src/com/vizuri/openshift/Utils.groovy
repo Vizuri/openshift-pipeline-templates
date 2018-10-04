0771291085f276b66dd13a8cc7b9debda6bb407b
Kent Eudy <keudy@vizuri.com>
2018-10-04 07:56:37 -0400

    Update

--- src/com/vizuri/openshift/Utils.groovy
+++ src/com/vizuri/openshift/Utils.groovy
@@ -68,6 +68,22 @@ def dockerBuildOpenshift(ocp_cluster, ocp_project, app_name) {
 				if(bc.status != 0 ) {
 					return false
 				}
+				
+				
+				builds.untilEach(1) {
+					echo "In Look for bc status:" + it.count()
+					def allDone = true
+					it.withEach {
+						// 'it' is now bound to a Selector selecting a single object for this iteration.
+						// Let's model it in Groovy to check its status.
+						echo "Checking build: "  + it.object().status.phase
+						def buildModel = it.object()
+						if ( it.object().status.phase != "Complete" ) {
+							allDone = false
+						}
+					}
+					return allDone
+				}
 
 //				def builds = bc.related('builds')
 //				timeout(5) {
