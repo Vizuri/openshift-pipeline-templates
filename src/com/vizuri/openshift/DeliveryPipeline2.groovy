2602643ec05c5accf44de93b1dfa77bff14ac763
Kent Eudy <keudy@vizuri.com>
2018-10-25 15:28:49 -0400

    update

--- src/com/vizuri/openshift/DeliveryPipeline2.groovy
+++ src/com/vizuri/openshift/DeliveryPipeline2.groovy
@@ -83,8 +83,8 @@ def call(body) {
 					deleteDir()
 					unstash 'artifacts'
 					img = utils.dockerBuild(pipelineParams.app_name, release_number)
-					utils.scanImage(pipelineParams.app_name, release_number )				
 					utils.dockerPush(img)
+					utils.scanImage(pipelineParams.app_name, release_number )				
 					stage('Confirm Deploy?') {
 						utils.notify("cicd-test", "Release ${release_number} of ${pipelineParams.app_name} is ready for test test. Promote release here ${JOB_URL}")
 						input message: "Do you want to deploy ${pipelineParams.app_name} release ${release_number} to test?", submitter: "keudy"
