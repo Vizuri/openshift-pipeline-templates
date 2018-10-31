cc7ab7beea70ca8b9f2cf0144c0e28312771b2fa
Kent Eudy <keudy@vizuri.com>
2018-10-31 10:41:20 -0400

    updates

--- src/com/vizuri/openshift/Utils.groovy
+++ src/com/vizuri/openshift/Utils.groovy
@@ -10,6 +10,12 @@ class Globals {
 }
 
 def init(projectFolder = "./") {
+	tools {
+		maven 'maven'
+		jdk 'jdk'
+		oc 'oc'
+		git 'git'
+	}
 	node {
 		echo ">>>>>>  Branch Name: " + BRANCH_NAME;
 		def release_number;
