43bb927646e3acdd8bc1c0987c65988fc9cdfebd
Kent Eudy <keudy@vizuri.com>
2018-11-01 10:57:54 -0400

    update

--- src/com/vizuri/openshift/JavaDeliveryPipeline.groovy
+++ src/com/vizuri/openshift/JavaDeliveryPipeline.groovy
@@ -67,8 +67,10 @@ def call(body) {
 					utils.scanImage(pipelineParams.app_name, projectFolder )	
 					utils.confirmDeploy(pipelineParams.app_name,pipelineParams.ocp_test_project)			
 					utils.deployOpenshift(pipelineParams.ocp_test_cluster, pipelineParams.ocp_test_project, pipelineParams.app_name  )
+					utils.integrationTestJava(pipelineParams.app_name, pipelineParams.ocp_test_project, projectFolder)
 					utils.confirmDeploy(pipelineParams.app_name,pipelineParams.ocp_prod_project)			
 					utils.deployOpenshift(pipelineParams.ocp_prod_cluster, pipelineParams.ocp_prod_project, pipelineParams.app_name  )
+					utils.integrationTestJava(pipelineParams.app_name, pipelineParams.ocp_prod_project, projectFolder)	
 				}
 			}
 		} catch (e) {
