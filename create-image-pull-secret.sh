8a3f01cc3034a8a484297b913d108a20c5d91a41
Kent Eudy <keudy@vizuri.com>
2018-11-06 20:46:11 -0500

    update

--- create-image-pull-secret.sh
+++
@@ -1,9 +0,0 @@
-oc create secret docker-registry quay-registry \
-    --docker-server=52.91.247.224:30080 \
-    --docker-username=admin \
-    --docker-password=P@ssw0rd \
-    --docker-email=admin@vizuri.com
-
-
-
-oc secrets link deployer quay-registry --for=pull
